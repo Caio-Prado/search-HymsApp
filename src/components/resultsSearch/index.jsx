fb7425017f466a76bde492dd3cf15c1920844972
Caio-Prado <caio100xa@gmail.com>
2022-05-20 08:19:41 -0300

    adicionando array de hinos

--- src/components/resultsSearch/index.jsx
+++ src/components/resultsSearch/index.jsx
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+import React from 'react';
+import { hinos } from '../../mocks/correlacaoDeHinos';
+import './styles.css'
+
+const resultsSearch = (hym, chooseHym) => {
+    
+    return (
+        <div>
+            {hinos.map((hino) => (
+            <table key={hino.id} className="container">
+                <tbody>
+                    <tr>
+                        <td>Correlação</td>
+                        <td className="title">Hinário 5</td>
+                        <td className="title">Hinário 4</td>
+                    </tr>
+                    <tr>
+                        <td>Hino</td>
+                        <td>{hino.id}</td>
+                        <td>{hino.hinario4}</td>
+                    </tr>
+                    <tr>
+                        <td>Título</td>
+                        <td>{hino.tituloH5}</td>
+                        <td>{hino.tituloH4}</td>
+                    </tr>
+                    <tr>
+                        <td>Acidentes</td>
+                        <td>{hino.acidentesH5}</td>
+                        <td>{hino.acidentesH4}</td>
+                    </tr>
+                <tr>
+                        <td>Tonalidade</td>
+                        <td>{hino.tonalidadeH5}</td>
+                        <td>{hino.tonalidadeH4}</td>
+                    </tr>
+                <tr>
+                        <td>Compasso</td>
+                        <td>{hino.compassoH5}</td>
+                        <td>{hino.compassoH4}</td>
+                    </tr>
+                <tr>
+                        <td>Autores</td>
+                        <td>{hino.autoresH5}</td>
+                        <td>{hino.autoresH4}</td>
+                    </tr>
+                </tbody>
+            </table>
+            ))}
+        </div>
+    )
+}
+
+=======
 import React from 'react';
 import dataHyms from '../../mocks/correlacaoHinosCCB.json';
 
@@ -50,4 +105,5 @@ const resultsSearch = (props) => {
     )
 }
 
+>>>>>>> 3baedfe9110a01e10a07f7e2d61987691762d165
 export default resultsSearch;
